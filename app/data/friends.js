b007a1767fcd01f69d1f0beb4b0c83c49ca5e64b
Tristan Martin <smove50@gmail.com>
2019-03-04 02:02:20 -0500

    sending homework in

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+var friends = [{
+        "name":"Man With Beard",
+        "photo":"https://unsplash.com/photos/_4gaNP2KI84",
+        "answers[]":["5","2","3","4","2","4","1","3","5","4"]
+    },{
+        "name":"Woman wearing Denim Jacket",
+        "photo":"https://unsplash.com/photos/W7b3eDUb_2I",
+        "answers[]":["5","5","4","2","3","1","4","5","2","5"]
+    },{
+        "name":"Michael",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAzNAAAAJDk4YmFiODRmLTRmYTUtNGU1NC1hMThmLTM1OTExNTcwYTdlYQ.jpg",
+        "answers[]":["2","4","1","2","2","1","2","5","5","3"]
+    },{
+        "name":"Joe",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAzNAAAAJDk4YmFiODRmLTRmYTUtNGU1NC1hMThmLTM1OTExNTcwYTdlYQ.jpg",
+        "answers[]":["1","5","5","1","4","3","2","2","5","3"]
+    },{
+        "name":"Jessica",
+        "photo":"https://unsplash.com/photos/THIs-cpyebg",
+        "answers[]":["4","3","2","3","4","1","3","5","5","2"]
+    },{
+        "name":"Steve",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAzNAAAAJDk4YmFiODRmLTRmYTUtNGU1NC1hMThmLTM1OTExNTcwYTdlYQ.jpg",
+        "answers[]":["5","4","1","2","3","1","4","3","2","5"]
+    },{
+        "name":"Derek",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAzNAAAAJDk4YmFiODRmLTRmYTUtNGU1NC1hMThmLTM1OTExNTcwYTdlYQ.jpg",
+        "answers[]":["3","2","4","3","2","4","5","5","2","4"]
+    },{
+        "name":"Lynn",
+        "photo":"https://unsplash.com/photos/7MrDe9XWSag",
+        "answers[]":["1","4","5","2","2","5","2","4","3","4"]
+    },{
+        "name":"Derek Haworth",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAzNAAAAJDk4YmFiODRmLTRmYTUtNGU1NC1hMThmLTM1OTExNTcwYTdlYQ.jpg",
+        "answers[]":["2","2","4","3","3","3","3","3","3","1"]
+    },{
+        "name":"Burt Macklin",
+        "photo":"https://vignette.wikia.nocookie.net/parksandrecreation/images/e/ea/Burt_Macklin.png/revision/latest?cb=20130520202236",
+        "answers[]":["3","2","5","2","3","5","4","5","1","2"]
+    }
+
+];
+module.exports = friends;
